76d72549f3bfe9c611b326c52717b1f26532fcfc
anirban <aroy0006@student.monash.edu>
2021-03-26 18:25:34 +1100

    Inital files for maps funcitonality

--- homeapp/views.py
+++ homeapp/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-    return render(request,"home.html",{})
+    return render(request,"maps.html",{})
