485d863a57f8e282ca3268d6da800511940e12f3
anirban <aroy0006@student.monash.edu>
2021-03-26 20:00:24 +1100

    Working map showing route between 2 points

--- static/js/mapInit.js
+++ static/js/mapInit.js
@@ -1,8 +1,46 @@
 let map;
-
+//Funciton to initalize map
 function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
+    //initalize google services
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
+    map = new google.maps.Map(document.getElementById("map"), {
+        // Melbounre lat and long
+        center: { lat: -37.8136, lng: 144.9631 },
+        zoom: 10,
+    });
+    //set map on init
+    directionsRenderer.setMap(map)
+    // call onchange function whenever start and end values change
+    const onChangeHandler = function () {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    }
+    //listeners for change event on the input fields
+    document.getElementById("start").addEventListener("change", onChangeHandler);
+    document.getElementById("end").addEventListener("change", onChangeHandler);
+
+}
+//Function to calculate distance between start and end
+function calculateAndDisplayRoute(directionsService, directionsRenderer){
+    //Set route details and settings
+    directionsService.route(
+        {
+            origin: {
+                query: document.getElementById("start").value,
+            },
+            destination: {
+                query: document.getElementById("end").value,
+            },
+            travelMode: google.maps.TravelMode.DRIVING,
+        },
+        // check for response and status
+        (response, status) => {
+            if(status === "OK") {
+                // set the directions
+                directionsRenderer.setDirections(response);
+            } else {
+                window.alert("Direction request failed"+status);
+            }
+        }
+    );
 }
