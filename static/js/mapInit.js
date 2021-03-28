2bbc9b319d704bd9c94a202ccd35ec044f656fd2
anirban <aroy0006@student.monash.edu>
2021-03-28 23:32:47 +1100

    Created class for autocompletion; fixed locate me bug; cleaned up the code with comments

--- static/js/mapInit.js
+++ static/js/mapInit.js
@@ -2,100 +2,150 @@
 // parameter when you first load the API. For example:
 // <script
 // src="https://maps.googleapis.com/maps/api/js?key=YOUR_API_KEY&libraries=places">
+let infoWindow;
 function initMap() {
-  const map = new google.maps.Map(document.getElementById("map"), {
-    mapTypeControl: false,
-    center: { lat: -33.8688, lng: 151.2195 },
-    zoom: 13,
-  });
-  new AutocompleteDirectionsHandler(map);
-}
-
-class AutocompleteDirectionsHandler {
-  constructor(map) {
-    this.map = map;
-    this.originPlaceId = "";
-    this.destinationPlaceId = "";
-    this.travelMode = google.maps.TravelMode.WALKING;
-    this.directionsService = new google.maps.DirectionsService();
-    this.directionsRenderer = new google.maps.DirectionsRenderer();
-    this.directionsRenderer.setMap(map);
-    const originInput = document.getElementById("origin-input");
-    const destinationInput = document.getElementById("destination-input");
-    const modeSelector = document.getElementById("mode-selector");
-    const originAutocomplete = new google.maps.places.Autocomplete(originInput);
-    // Specify just the place data fields that you need.
-    originAutocomplete.setFields(["place_id"]);
-    const destinationAutocomplete = new google.maps.places.Autocomplete(
-      destinationInput
-    );
-    // Specify just the place data fields that you need.
-    destinationAutocomplete.setFields(["place_id"]);
-    this.setupClickListener(
-      "changemode-walking",
-      google.maps.TravelMode.WALKING
-    );
-    this.setupClickListener(
-      "changemode-transit",
-      google.maps.TravelMode.TRANSIT
-    );
-    this.setupClickListener(
-      "changemode-driving",
-      google.maps.TravelMode.DRIVING
-    );
-    this.setupPlaceChangedListener(originAutocomplete, "ORIG");
-    this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(
-      destinationInput
-    );
-    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
-  }
-  // Sets a listener on a radio button to change the filter type on Places
-  // Autocomplete.
-  setupClickListener(id, mode) {
-    const radioButton = document.getElementById(id);
-    radioButton.addEventListener("click", () => {
-      this.travelMode = mode;
-      this.route();
+    //create a map centred on melbourne
+    const map = new google.maps.Map(document.getElementById("map"), {
+      mapTypeControl: false,
+      center: { lat: -37.8136, lng: 144.9631 },
+      zoom: 13,
     });
-  }
-  setupPlaceChangedListener(autocomplete, mode) {
-    autocomplete.bindTo("bounds", this.map);
-    autocomplete.addListener("place_changed", () => {
-      const place = autocomplete.getPlace();
-
-      if (!place.place_id) {
-        window.alert("Please select an option from the dropdown list.");
-        return;
-      }
-
-      if (mode === "ORIG") {
-        this.originPlaceId = place.place_id;
-      } else {
-        this.destinationPlaceId = place.place_id;
-      }
-      this.route();
-    });
-  }
-  route() {
-    if (!this.originPlaceId || !this.destinationPlaceId) {
-      return;
+    //create a infowindow for locate me services
+    infoWindow = new google.maps.InfoWindow()
+    //create the locate me button
+    const locationButton = document.createElement("button");
+    //Add text to the button
+    locationButton.textContent = "Locate me";
+    //Add class
+    locationButton.classList.add("custom-map-control-button");
+    //Push the button to the top left corner
+    map.controls[google.maps.ControlPosition.TOP_LEFT].push(locationButton);
+    //Set up listener
+    const onLocationChangeHandler = function () {
+        getCurrentLocation(map)
     }
-    const me = this;
-    this.directionsService.route(
-      {
-        origin: { placeId: this.originPlaceId },
-        destination: { placeId: this.destinationPlaceId },
-        travelMode: this.travelMode,
-      },
-      (response, status) => {
-        if (status === "OK") {
-          me.directionsRenderer.setDirections(response);
-        } else {
-          window.alert("Directions request failed due to " + status);
-        }
-      }
-    );
+    //Add listener
+    locationButton.addEventListener("click",onLocationChangeHandler);
+    //Create a new autocomplete handler
+    new AutocompleteDirectionsHandler(map);
   }
+  
+  //Class for auto complete direction handlers
+  class AutocompleteDirectionsHandler {
+    //PARAM: map; Google Map Type
+    //Creates a map with autocomplete suggestions
+    constructor(map) {
+        //Set initial values.
+        //Map to be shown
+        this.map = map;
+        //Origin ID
+        this.originPlaceId = "";
+        //Destination ID
+        this.destinationPlaceId = "";
+        //Define travel mode
+        this.travelMode = google.maps.TravelMode.DRIVING;
+        //Declare direction and render services
+        this.directionsService = new google.maps.DirectionsService();
+        this.directionsRenderer = new google.maps.DirectionsRenderer();
+        //set the direction renderer on the map
+        this.directionsRenderer.setMap(map);
+        //Get origin input
+        const originInput = document.getElementById("origin-input");
+        //Get destination input
+        const destinationInput = document.getElementById("destination-input");
+        //Set autocomplete on origin
+        const originAutocomplete = new google.maps.places.Autocomplete(originInput);
+        //Specify just the place data fields that you need.
+        originAutocomplete.setFields(["place_id"]);
+        //Set autocomplete for destination
+        const destinationAutocomplete = new google.maps.places.Autocomplete(destinationInput);
+        // Specify just the place data fields that you need.
+        destinationAutocomplete.setFields(["place_id"]);
+        //Setting up  listeners
+        this.setupPlaceChangedListener(originAutocomplete, "ORIG");
+        this.setupPlaceChangedListener(destinationAutocomplete, "DEST");
+        //Setting search boxes to top left.
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+        this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+        }
+        //PARAM:autocomplete obj, origin or destination mode
+        //Function to setup listeners to the text fields
+        setupPlaceChangedListener(autocomplete, mode) {
+        //bind the autocomplete to this map
+        autocomplete.bindTo("bounds", this.map);
+        //add a listener for autocomplete
+        autocomplete.addListener("place_changed", () => {
+            //Get the place suggested by auto complete
+            const place = autocomplete.getPlace();
+
+                if (!place.place_id) {
+                    window.alert("Please select an option from the dropdown list.");
+                    return;
+                }
+                //check if autocomplete is on origion or destination text box
+                if (mode === "ORIG") {
+                    //set on origion text box
+                    this.originPlaceId = place.place_id;
+                } else {
+                    //set on destination text box
+                    this.destinationPlaceId = place.place_id;
+                }
+                //call the route function when change is detected
+                this.route();
+            });
+        }
+        //Function to map and display the route on the map
+        route() {
+            //Check if both the origin and destination is in place, if not return
+            if (!this.originPlaceId || !this.destinationPlaceId) {
+                return;
+            }
+            const me = this;
+            // Call the route function
+            this.directionsService.route(
+                {
+                    //set required inputs
+                    origin: { placeId: this.originPlaceId },
+                    destination: { placeId: this.destinationPlaceId },
+                    travelMode: this.travelMode,
+                },
+                //Checkf for the response
+                (response, status) => {
+                    //if status ok
+                    if (status === "OK") {
+                        //Render the route received
+                        me.directionsRenderer.setDirections(response);
+                    } else {
+                        //Displa alert
+                        window.alert("Directions request failed due to " + status);
+                    }
+                }
+            );
+        }   
+  }
+
+//PARAM: map object
+//Fetch current location, throw error if permission not granted
+function getCurrentLocation(map){
+    //Check permission
+    if (navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            (position) => {
+                // Current location lat & Lng
+                const pos = {
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude,
+                };
+                //Set position
+                infoWindow.setPosition(pos);
+                infoWindow.setContent("Location Found");
+                // Set on map
+                infoWindow.open(map);
+                // Center map on location
+                map.setCenter(pos);
+            }, () => {
+                handleLocationError(false, infoWindow, map.getCenter());
+            }
+        )
+    }
 }
