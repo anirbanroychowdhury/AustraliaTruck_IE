51054e769b1b0c3c08e8c7a71187c49ea57ec8ad
anirban <aroy0006@student.monash.edu>
2021-04-12 01:31:30 +1000

    Added comments for usability

--- facedetectionapp/camera.py
+++ facedetectionapp/camera.py
@@ -1,32 +1,48 @@
+# Name: Camera Queue
+# Main Job: initaite a camera at the start of app opening, it will create a queue to display the frames in order
+#
+# Author: Anirban Roy Chowdhury
+# Craated Date: 30 March 2021
+# Version: 1.1.0
+# ClassID# 1000
+
+# Class Usage
+#   1. Camera class is used to manage the frames and its respective queueu
+
 import threading
 from time import sleep
 
 
 class Camera(object):
+    #Creates a Camera object
     def __init__(self, process):
         self.to_process = []
         self.to_output = []
         self.process = process
-
+        #Create and start a thread when an pbject is initiated
         thread = threading.Thread(target=self.keep_processing, args=())
         thread.daemon = True
         thread.start()
 
+    #if the queue has a frame send the frame to be displayed
     def process_one(self):
         if not self.to_process:
             return
         camera_frame = self.to_process.pop(0)
         output_frame = self.process.process(camera_frame)
         self.to_output.append(output_frame)
-
+    
+    #keep sending the images
     def keep_processing(self):
         while True:
             self.process_one()
             # sleep(0.01)
 
+    #Add an image to the queue
     def enqueue_input(self, input):
         self.to_process.append(input)
 
+    #Get the image to be displayed if the queue is not empty
     def get_frame(self):
         while not self.to_output:
             sleep(0.01)
