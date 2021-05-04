1ebc72fdc60cefd75c51edbed5cb55295d065375
anirban <aroy0006@student.monash.edu>
2021-05-04 23:04:34 +1000

    basic changes

--- facedetectionapp/views.py
+++ facedetectionapp/views.py
@@ -21,30 +21,50 @@ from django.core.files.base import ContentFile
 
 #Get user camera through javascript
 # webopencv() function to initialize class from process
-camera = Camera(webopencv())
 
+# camera = Camera(webopencv())
+cameraList = {}
 
 def index_view(request, *args, **kwargs):
-    """Video streaming home page."""
-    return render(request,'faceDetect.html',{})
-
+	request.session.create()
+	try:
+		sessionID = request.session.session_key
+		if sessionID not in cameraList:
+			camera = Camera(webopencv(),sessionID)
+			cameraList[sessionID] = camera
+		return render(request,'faceDetect.html',{})
+	except Exception as e:
+		print(e)
 
 #Converts the gives file into base64 and enques it into the image queue
 @csrf_exempt
 def video_feed(request, *args, **kwargs):
 	#IF request is POST
-	if request.method == 'POST':
-		_format, _data = str(request.body).split(';base64,')
-		#Convert the string into an image
-		file = ContentFile( base64.b64decode(_data))
-		#Open image
-		img = Image.open(file)
-		#Enqueue
-		camera.enqueue_input(img)
-		#Get processed image
-		processedResult = camera.get_frame()
-		#Create dict for response
-		res = {'camera_frame':processedResult[0],'alarm':processedResult[1]}
-		#Convert to Json and send response
-		return HttpResponse(json.dumps(res),content_type='application/json')
+	try:
+		if request.method == 'POST':
+			sessionID = request.session.session_key
+			if sessionID in cameraList:
+				print("Gettting camera from list")
+				camera = cameraList[sessionID]
+			else:
+				print("creating camera and appending to dict from video_feed")
+				camera = Camera(webopencv(),sessionID)
+				cameraList[sessionID] = camera
+			print(camera.getID())
+			_format, _data = str(request.body).split(';base64,')
+			#Convert the string into an image
+			file = ContentFile( base64.b64decode(_data))
+			#Open image
+			img = Image.open(file)
+			#Enqueue
+			camera.enqueue_input(img)
+			#Get processed image
+			processedResult = camera.get_frame()
+			#Create dict for response
+			res = {'camera_frame':processedResult[0],'alarm':processedResult[1]}
+			#Convert to Json and send response
+			return HttpResponse(json.dumps(res),content_type='application/json')
+	except Exception as e:
+		print(e)
+
         
