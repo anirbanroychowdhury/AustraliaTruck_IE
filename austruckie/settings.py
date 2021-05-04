bbefab58b6e74c91878ed4ef1204fc6e903871cb
anirban <aroy0006@student.monash.edu>
2021-05-05 00:38:23 +1000

    adding database engine

--- austruckie/settings.py
+++ austruckie/settings.py
@@ -10,6 +10,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
+import dj_database_url
+import dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -76,12 +78,15 @@ WSGI_APPLICATION = 'austruckie.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(conn_max_age=600,ssl_require=True)
+SESSION_ENGINE= 'django.contrib.sessions.backends.cached_db'
 
 
 # Password validation
